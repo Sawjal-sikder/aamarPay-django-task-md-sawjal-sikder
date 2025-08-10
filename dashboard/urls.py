f9a7272e21f8ba90f939e8905c941b5d5c6fe983
Sawjal <mdsajalsikder@gmail.com>
2025-08-10 17:58:51 +0600

    Add login and logout views, update templates and URLs for authentication

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('home/', home, name='dashboard'),
+    path('login/', LoginView, name='LoginView'),
+    path('logout/', LogoutView, name='LogoutView'),
     path("transition/",payment_history, name="transition")
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def LoginView(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')  # or any page you want after login
+        else:
+            messages.error(request, "Invalid username or password.")
+    return render(request, "admin/login.html")
+
+def LogoutView(request):
+    logout(request)
+    return redirect("LoginView")
+
+@login_required(login_url='login')
 def home(request):
     return render(request, 'main.html')
-
+@login_required(login_url='login')
 def payment_history(request):
     return render(request, "dashboard/payment_history.html")
